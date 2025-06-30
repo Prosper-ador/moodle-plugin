acf747fdc4560cad4fb6454db693336d24642ef2
prosper-ador <prosnyengka@gmail.com>
2025-06-30 09:47:57 +0100

    fix: update working directory and authentication token for GitHub Packages in workflow

--- hello_world_lib/main.js
+++ hello_world_lib/main.js
@@ -1,10 +1,11 @@
 import init, { greet } from './pkg/hello_world_lib.js';
 
-export const initRust = async () => {
-  await init('/local/wasmplugin/amd/src/hello_world_lib_bg.wasm');
-  const msg = greet("Moodle from Rust");
-  const div = document.getElementById("rust-output");
-  if (div) div.textContent = msg;
-};
+function initRust() {
+  return init('/local/wasmplugin/amd/src/hello_world_lib_bg.wasm').then(() => {
+    const msg = greet("Moodle from Rust");
+    const div = document.getElementById("rust-output");
+    if (div) div.textContent = msg;
+  });
+}
 
-initRust();
+export { initRust };
